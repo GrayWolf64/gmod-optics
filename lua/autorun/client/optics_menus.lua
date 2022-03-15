774fc992ad202babf16efb179012f16a5ef20e1e
GrayWolf <88234821+GrayWolf64@users.noreply.github.com>
2022-03-15 21:43:53 +0800

    Add files via upload

---
+++ lua/autorun/client/optics_menus.lua
@@ -0,0 +1,19 @@
+hook.Add( "AddToolMenuTabs", "opticstab", function()
+	spawnmenu.AddToolTab( "Optics", "Optics", "icon16/color_wheel.png" )
+end )
+local function CreateOpticsCategories()
+
+        spawnmenu.AddToolCategory( "Optics", "Lens", "Lens" )
+		
+        spawnmenu.AddToolCategory( "Optics", "Reflectors", "Reflectors" )
+
+        spawnmenu.AddToolCategory( "Optics", "Prisms", "Prisms" )
+        
+        spawnmenu.AddToolCategory( "Optics", "Calculations", "Calculations" )
+
+        spawnmenu.AddToolCategory( "Optics", "Settings", "Settings" )
+        
+
+end        
+hook.Add( "AddToolMenuCategories", "CreateOpticsCategories", CreateOpticsCategories )
+
