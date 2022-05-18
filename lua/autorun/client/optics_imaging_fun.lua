d3b142877aee6201412dcc0d4d85e8f2c1ceac2f
GrayWolf <88234821+GrayWolf64@users.noreply.github.com>
2022-05-18 20:41:54 +0800

    Add files via upload

--- lua/autorun/client/optics_imaging_fun.lua
+++ lua/autorun/client/optics_imaging_fun.lua
@@ -6,27 +6,35 @@ if CLIENT then
 
       local lensindex = lens:EntIndex()
       local angles = lens:GetAngles()
-      //local pos_front_processed_world = lens:LocalToWorld(pos_front_processed)
-      local angles_front_processed_world = lens:LocalToWorldAngles(angles)
       local pos_direct = lens:GetPos()
       local tar = (ConcaveLensTraces_Front_Table[lensindex]).Entity
-      print((tar:GetAngles():Right() * -90).x,(tar:GetAngles():Right() * -90).y)
-      local w, h = ScrW(), ScrH()
-
+      local w, h = 1024, 1024
       local imagingrt1 = GetRenderTarget( "optics_rt_id1", w, h, true )
 
-      cam.Start3D(pos_direct, Angle(-0, -90), fov == 90)
+      if tar:IsValid() == true then
+         local rendermode_old = tar:GetRenderMode()
+         local filename0 = string.gsub("concavelens#INDEX#_imaging_result_front.png","#INDEX#",tostring(lensindex))
+
+       cam.Start3D(pos_direct,angles:Right():Angle(),90)
          render.PushRenderTarget( imagingrt1 )
-         render.Clear( 0, 255, 64, 255, true )
-         if tar:IsValid() == true and tar:GetClass() == "prop_physics" then
+         render.Clear( 255, 255, 255, 0, true )
+
+         if tar:GetClass() == "prop_physics" then
+
+          tar:SetRenderMode(RENDERMODE_NONE)
+
           tar:DrawModel()
-          local data1 = render.Capture({ format = "jpeg", quality = 40, x = 0, y = 0, w = w, h = h })
-          local file1 = file.Open( "cc_imaging_result_front_ent.jpg", "wb", "DATA" )
+          local data1 = render.Capture({ format = "png", quality = 50, x = 0, y = 0, w = w, h = h, alpha = true })
+          local file1 = file.Open( filename0, "wb", "DATA" )
           file1:Write( data1 )
           file1:Close()
+          tar:SetRenderMode(rendermode_old)
          end
+
          render.PopRenderTarget()
-      cam.End3D()
+       cam.End3D()
+      end
+
 
    end
 
@@ -35,24 +43,3 @@ if CLIENT then
    end
 
 end
-
---[[local w, h = ScrW(), ScrH()
-local customRt = GetRenderTarget( "some_unique_render_target_nameeeee", w, h, true )
-cam.Start3D()
-render.PushRenderTarget( customRt )
-
-for key, prop in ipairs( ents.FindByClass( "prop_physics" ) ) do
-   prop:DrawModel()
-end
-
-	local data = render.Capture( {
-		format = "png",
-		x = 0,
-		y = 0,
-		w = ScrW(),
-		h = ScrH()
-	} )
-
-	file.Write( "image.png", data )
-render.PopRenderTarget()
-cam.End3D()--]]
